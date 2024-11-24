576ac6103dc92e2de30c45ae99cbae49a1f927e4
parisianelegancestaffing <parisianelegancestaffing@gmail.com>
2024-11-24 19:47:40 +0000

    Update script4.js

--- script4.js
+++ script4.js
@@ -152,8 +152,8 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault(); // Prevent default form submission
 
         let params = {
-            senderName: document.getElementById('name').value,
-            senderEmail: document.getElementById('email').value,
+            from_name: document.getElementById('name').value,
+            reply_to: document.getElementById('email').value,
             subject: document.getElementById('subject').value,
             message: document.getElementById('message').value,
         }
